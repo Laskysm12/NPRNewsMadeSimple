892a0b6d40be43c81c0a5630f71ca5ba5e19e2c3
Sam Lasky <Laskysm@gmail.com>
2018-04-18 17:00:39 -0400

    successful index.handlebars connection to server

--- controllers/controller.js
+++ controllers/controller.js
@@ -4,8 +4,10 @@ var router = express.Router();
 
 var db = require("../models");
 
-// router.get('/', function(req, res) {
+// Create all of our routes and set up logic within those routes when required
+router.get('/', function(req, res) {
+    res.render("index"); // Need to change!!
 
-// }); // End of router .get
+}); // End of router .get
 
 module.exports = router;
